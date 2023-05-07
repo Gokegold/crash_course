5f3b392ae786843617d3dc80a2bd05af5ab17646
Dr-Positive <116766276+Gokegold@users.noreply.github.com>
2023-05-08 00:52:23 +0100

    Update OOP_classes_and_instance.py

--- OOP_classes_and_instance.py
+++ OOP_classes_and_instance.py
@@ -1,25 +1,9 @@
-x = 1
-print(type("HELLO"))
-# 'hello' is an object of the class string
 
-
-def hello():
-  print('hello')
-# Object of the class function
-
-#methods can be preformed on object or acts on object
-
-string = "hello"
-print(string.upper())
-
-# the class function is like a blueprint for a method
-# a method is a function that goes inside a class, and alway begins with a parameter slef.
-# class can both store method ad store actions to be taken
-# we can call for the actions in a class
-#... By calling instance.themethod() and print no need to pass any agument into the method 
-#.... Or call the class.themethod(and pass the instance here)
 
 class Employee:
+
+    num_of_emps = 0
+    raise_amt = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
@@ -27,28 +11,49 @@ class Employee:
         self.pay = pay
         self.email = first + '.' + last + '@core.com'
 
+        Employee.num_of_emps += 1
+
     def fullname(self):
         return '{} ' '{}'.format(self.first, self.last)
+
+    def appy_raise(self):
+        # when accessing class variable or variable in a class we can do that through
+        # Class itself (in this case its Employee) OR the instance of the class (in the case it's the 'self')
+        # leaving it as self.
+        self.pay = int(self.pay * self.raise_amt)
+
+    # to turn a regular method to a Class_methods you use the decorator 'classmethod'
+    #
+    @classmethod
+    # the conventional method takes the instance as the first argument which is 'self'
+    # But class_method uses the cls instance
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
 
 
 emp_1 = Employee('Sam', 'Bents', 500000)
 emp_2 = Employee('Samon', 'Bent', 700000)
 
+
+print(emp_2.__dict__)
+print(emp_1.raise_amt)
+print(Employee.raise_amt)
+print(Employee.num_of_emps)
+
+"""
 print(emp_2.fullname())
 print(emp_2.email)
 print(' ')
-print(Employee.fullname(emp_1))
+print(Employee.fullname(emp_E1))
 print(emp_1.email)
 
 
 # OR
 
-'''
-class Employee:
-    
-    def fullname(self):
-        return '{} ' '{}'.format(self.first, self.last)
 
+class Employee:
+    def fullname(self):
+    return '{} ' '{}'.format(self.first, self.last)
 
 
 emp_1.first = 'Cort'
@@ -64,4 +69,10 @@ emp_2.pay = 700000
 emp_1 = Employee()
 emp_2 = Employee()
 
-'''
+"""
+
+Employee.set_raise_amt(2.00)
+
+print(Employee.raise_amt)
+print(emp_1.raise_amt)
+print(emp_2.raise_amt)
