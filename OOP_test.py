4ff53084e2cc7b866a1a347efe8dc0cf3f6df6ae
Dr-Positive <116766276+Gokegold@users.noreply.github.com>
2023-05-10 00:26:30 +0100

    Update OOP_test.py

--- OOP_test.py
+++ OOP_test.py
@@ -254,6 +254,19 @@ obj2.limo_info('GAC')
 # EXAMPLE:
 
 # Parent class
+class Company:
+    def company_name(self):
+        return 'Google'
+
+class Employee(Company):
+    def info(self):
+        # Calling the superclass method using super()function
+        c_name = super().company_name()
+        print("Jessa works at", c_name)
+
+# Creating object of child class
+emp = Employee()
+emp.info()
 '''
 class Carriage:
     def carriage_info(self):
