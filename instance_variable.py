78a34a076bbb21c03024c55a17571410e7c17d44
Dr-Positive <116766276+Gokegold@users.noreply.github.com>
2023-05-10 23:37:20 +0100

    Update instance_variable.py

--- instance_variable.py
+++ instance_variable.py
@@ -9,7 +9,7 @@ Github: https://www.github.com/Gokegold
 
 Date Created: April 28, 2023
 
-last modification:: [May 8, 2023],[May 7, 2023],[May 7, 2023]
+last modification:: [May 8, 2023],[May 9, 2023],[May 10, 2023]
 
 """
 # Instance variables are declared inside a method using the self keyword.
@@ -115,3 +115,53 @@ pupil = Pupils("Jesse", 20)
 # Use getattr() to access the values of the instance variable
 print('Name: ', getattr(pupil, 'noms'))
 print("AGE: ", getattr(pupil, 'ages'))
+
+
+# INSTANCE VARIABLES NAMING CONVENTIONS
+"""
+    Instance variable names should be all lower case. For example, 'id'
+    Words in an instance variable name should be separated by an underscore.
+    # FOR EXAMPLE, store_name
+    Non-public instance variables should begin wth a single Underscore
+    if an instance needs to bee mangled, two underscores may begin its name
+"""
+# DYNAMICALLY ADD INSTANCE VARIABLE TO A OBJECT
+"""
+    We can add instance variables from the outside of class to a particular object.
+    Use the following syntax to add the new instance variable to the object
+    object_referance.variable_name = value
+"""
+
+
+# EXAMPLE:
+class Apprentice:
+    # Instance variable
+    def __init__(self, names, ages):
+        self.names = names
+        self.ages = ages
+
+
+# create object
+app = Apprentice('JONES', 21)
+
+print('Before')
+print('Name:', app.names, 'AGES:', app.ages)
+
+# add new instance variable 'scores' to app
+app.scores = 75
+print('After')
+print('Name:', app.names, 'AGES:', app.ages), 'SCORES:', app.scores)
+
+"""
+    ALL OUTPUT
+        Name:  Jesse
+        AGE:  20
+        
+        Before
+        Name: JONES AGES: 21
+        
+        After
+        Name: JONES AGES: 21
+        SCORES: 75
+"""
+
