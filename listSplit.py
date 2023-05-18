5b75c78881b0b51ed3b00596477882dd7fb0eb67
Dr-Positive <116766276+Gokegold@users.noreply.github.com>
2023-05-18 21:01:41 +0100

    Update listSplit.py

--- listSplit.py
+++ listSplit.py
@@ -1,3 +1,18 @@
+"""
+listsplit
+
+Twitter @i_amgoke: https://twitter.com/i_amgoke
+
+Github: https://www.github.com/Gokegold
+
+Date Created: May 18, 2023
+
+last modification: [May 18, 2023]
+
+"""
+
+
+
 def listSplit():
     global bookname
     global authorname
