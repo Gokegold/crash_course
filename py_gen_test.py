0fd51c97e4b6d13b96bb8cfc6d4e790b362495da
Dr-Positive <116766276+Gokegold@users.noreply.github.com>
2023-05-28 17:56:36 +0100

    Create py_gen_test.py

---
+++ py_gen_test.py
@@ -0,0 +1,29 @@
+"""
+Descrption: Basic Python program test
+
+Twitter @i_amgoke: https://twitter.com/i_amgoke
+
+Github: https://www.github.com/Gokegold
+
+Date Created: May 28, 2023
+
+last modification:: [May 28, 2023]
+
+"""
+# Question 1
+"""
+Given two numbers num1 and num2. 
+The task is to write a Python program to find the addition of these two numbers. 
+"""
+
+# method 1
+
+def add_two_num():
+  num1 = int(input("Enter a number: ")
+  num2 = int(input("Enter a number: ")
+  sum_of_num = num1 + num2
+  return sum_of_num
+
+
+print(add)
+
